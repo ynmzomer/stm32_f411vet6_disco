6c40832bfcbbf639e959c73738f2af27ccdae233
ynmzomer <168364597+ynmzomer@users.noreply.github.com>
2025-02-28 18:49:19 +0300

    Update 008i2c_master_tx_test.c

--- stm32f411_drivers/Src/008i2c_master_tx_test.c
+++ stm32f411_drivers/Src/008i2c_master_tx_test.c
@@ -2,7 +2,7 @@
  * File: 008i2c_master_tx_test.c
  * Description: STM32F411 driver test application.
  * Purpose: This file contains code to test custom driver implementations for GPIO, SPI, I2C, and UART.
- * Author: Betül Atalay
+ * Author: omery
  */
 
 /*
